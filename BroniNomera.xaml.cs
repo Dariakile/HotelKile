1a38a9aeb42bbcd3ad9f5229089b82a7d197a86e
u31111 <u31111@P31111>
2024-12-05 15:57:30 +1000

    Добавьте файлы проекта.

---
+++ BroniNomera.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelKile
+{
+    /// <summary>
+    /// Логика взаимодействия для BroniNomera.xaml
+    /// </summary>
+    public partial class BroniNomera : Page
+    {
+        public BroniNomera()
+        {
+            InitializeComponent();
+        }
+
+        private void NaitiNomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
